c46de22560104cdbb936cbdd65a3c433f631c558
pjanai <paulojanai.m.s@gmail.com>
2023-11-04 11:31:09 -0300

    feat: new error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
+
+import Button from '@components/Button';
 import GridContainer from '@components/Grid';
 
 function Error() {
   return (
     <div className='page-error'>
       <GridContainer>
-        <h1>Error page</h1>
+        <h1>Oops...</h1>
+        <h4>Página não encontrada</h4>
+        <Button href='/'>
+          <ArrowLeft size={24} />
+          <p className='body-m'>Voltar a página inicial</p>
+        </Button>
+        <Link to='/'></Link>
       </GridContainer>
     </div>
   );
