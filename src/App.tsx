1f6692c578caed327b40833650984a991b1092f7
pjanai <paulojanai.m.s@gmail.com>
2024-01-12 11:18:30 -0300

    feat: hotjar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import '@scss/main.scss';
 import router from '@routes/index.routes';
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3815671;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import Hotjar from '@hotjar/browser';
-
-const siteId = 3815671;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
